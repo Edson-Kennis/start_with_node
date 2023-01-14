37578c1d712292fc10b9bb2971388cf8d298d6e0
Edson Kennis <Edson.K.Santos@outlook.com>
2023-01-14 09:50:01 -0300

    Finalizando básico do básico sobre Rotas

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (require, response) => {
+    response.send("Bem vindo ao guia do programador")
+});
+app.get("/blog/:bobo?", (require, response) => {
+    response.send("Bem vindo ao meu blog")
+});
+app.get("/canal/youtube", (require, response) => {
+    let canal = require.query["canal"];
+    response.send(canal)
+});
+app.get("/ola/:nome", (require, response) => {
+    response.send("oi! " + require.params.nome)
+});
+
 app.listen(4000,function(error) {
     if(error) {
         console.log('Ocorreu um erro');
